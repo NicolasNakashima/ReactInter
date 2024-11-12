8b5f0ea8c34b61224f5ba8a3074b46e695d10647
NicolasNakashima <nicolas.nakashima@germinare.org.br>
2024-11-12 11:29:41 -0300

    dash

--- src/Pages/DashboardFair/index.tsx
+++ src/Pages/DashboardFair/index.tsx
@@ -4,7 +4,7 @@ export const DashboardFair = () => {
     return (
         <S.Wrapper>
             <S.Container>
-            <iframe title="Area restrita" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYjQ0NzQzN2ItZDg2Mi00MDEzLThkMWUtMmUwMWVjM2Q3Yzg0IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9" frameborder="0" allowFullScreen="true"></iframe>
+            <iframe title="Area restrita" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYjQ0NzQzN2ItZDg2Mi00MDEzLThkMWUtMmUwMWVjM2Q3Yzg0IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9"  allowFullScreen></iframe>
             </S.Container>
         </S.Wrapper>
     );
